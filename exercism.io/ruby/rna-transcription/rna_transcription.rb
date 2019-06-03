2e107bdcd999b122961d8d6bda2664b296d72865
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-06-03 18:41:24 +0100

    refactor to make solution more intuitive and reusable

--- exercism.io/ruby/rna-transcription/rna_transcription.rb
+++ exercism.io/ruby/rna-transcription/rna_transcription.rb
@@ -1,6 +1,13 @@
 module Complement
-  DNA = "GCTA"
-  RNA = "CGAU"
+  DNA_RNA_MAPPING = {
+    "G" => "C", 
+    "C" => "G", 
+    "T" => "A", 
+    "A" => "U"}
+
+  DNA = DNA_RNA_MAPPING.keys.join
+  RNA = DNA_RNA_MAPPING.values.join
+  
   def self.of_dna(dna)
     dna.tr(DNA, RNA)
   end
