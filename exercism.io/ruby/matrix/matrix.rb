4a555f6ecc7755751d653b70ad76ea24a1a885a1
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-06-03 18:39:28 +0100

    memoize the methods and use built-in Array#transpose to get @columns from @rows

--- exercism.io/ruby/matrix/matrix.rb
+++ exercism.io/ruby/matrix/matrix.rb
@@ -1,32 +1,20 @@
 class Matrix
-  attr_reader :rows, :columns
 
-  def initialize(lines)
-    matrix = transform(lines)
-    @rows = matrix[:rows]
-    @columns = matrix[:colums]
+  def initialize(string_matrix)
+    @string_matrix = string_matrix
+  end
+
+  def rows
+    @rows ||= transform
+  end
+
+  def columns
+    rows.transpose
   end
 
   private
-  def transform(lines)
-    rows = []
-    colums = []
-    lines.split("\n").map do |line|
-      rows << line.split().map { |digit| digit.to_i }
-    end
-    p rows
-    0.upto(rows[0].length - 1) do |idx|
-      temp = []
-      0.upto(rows.length - 1) do |x|
-        temp << rows[x][idx]
-      end
-      colums << temp
-    end
-    {rows: rows, colums: colums}
+
+  def transform
+    @string_matrix.lines.map { |line| line.split.map(&:to_i) }
   end
 end
-
-m = Matrix.new("1 2 3\n4 5 6\n7 8 9\n 8 7 6")
-puts ""
-p m.rows
-p m.columns
